187eaa3e68bdf213b66e5d534f03da1d5c2482a4
BrunoMatSS <b.mateus.silva@gmail.com>
2022-03-13 15:31:57 -0400

    Meu Projeto

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+const listaSelecaoPokemons = document.querySelectorAll('.pokemon')
+const pokemonsCard = document.querySelectorAll('.cartao-pokemon')  
+
+listaSelecaoPokemons.forEach(pokemon => {
+    pokemon.addEventListener('click', () => {
+        const cartaoPokemonAberto = document.querySelector('.aberto')
+        cartaoPokemonAberto.classList.remove('aberto')
+
+        const idPokemonSelecionado = pokemon.attributes.id.value
+
+        const cartaoPokemonParaAbrir = document.getElementById('cartao-' + idPokemonSelecionado)
+        cartaoPokemonParaAbrir.classList.add('aberto')
+
+        const pokemonAtivoListagem = document.querySelector('.ativo')
+        pokemonAtivoListagem.classList.remove('ativo')
+
+        const pokemonSelecionadoListagem = document.getElementById(idPokemonSelecionado)
+        pokemonSelecionadoListagem.classList.add('ativo')
+    })
+})
